979b2aff9067cc4f49dbe4aef7d8eeb38f0b8104
LucasChizzoli <chizzoli.lucas13@gmail.com>
2022-06-23 13:28:57 -0300

    Add github provider

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,11 +1,12 @@
 import NextAuth from 'next-auth';
-import EmailProvider from 'next-auth/providers/email';
+import GitHubProvider from 'next-auth/providers/github';
+
 export default NextAuth({
   // Configure one or more authentication providers
   providers: [
-    EmailProvider({
-      server: process.env.EMAIL_SERVER,
-      from: process.env.EMAIL_FROM,
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
     }),
   ],
 });
